be8bd01864d99ea0a7b965fc2c209bcf910efaf2
daleonv <daleon@ec.krugercorp.com>
2025-01-29 17:39:27 -0500

    Se implementa la arquitectura de microservicios

---
+++ microservice-person/src/test/java/com/ec/app/microservice_person/MicroservicePersonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ec.app.microservice_person;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicePersonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
