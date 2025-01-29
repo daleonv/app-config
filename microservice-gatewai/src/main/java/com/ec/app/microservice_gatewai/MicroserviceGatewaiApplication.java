d5cfcee264798614b3f12c3da0d58b403e5257c1
daleonv <daleon@ec.krugercorp.com>
2025-01-29 11:34:10 -0500

    Initial commit

---
+++ microservice-gatewai/src/main/java/com/ec/app/microservice_gatewai/MicroserviceGatewaiApplication.java
@@ -0,0 +1,13 @@
+package com.ec.app.microservice_gatewai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceGatewaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceGatewaiApplication.class, args);
+	}
+
+}
---
+++ microservice-gatewai/src/test/java/com/ec/app/microservice_gatewai/MicroserviceGatewaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ec.app.microservice_gatewai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceGatewaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
