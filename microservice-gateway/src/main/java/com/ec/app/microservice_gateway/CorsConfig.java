0581910e10a62053216cb15461cf7f74f080c6a2
daleonv <daleon@ec.krugercorp.com>
2025-02-13 10:46:47 -0500

    Configuración de Cors

--- microservice-gateway/src/main/java/com/ec/app/microservice_gateway/CorsConfig.java
+++ microservice-gateway/src/main/java/com/ec/app/microservice_gateway/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
     @Bean
     public CorsWebFilter corsFilter() {
         CorsConfiguration corsConfig = new CorsConfiguration();
-        corsConfig.setAllowedOrigins(List.of("http://localhost:4200")); // Permitir Angular
+        corsConfig.setAllowedOrigins(List.of("http://localhost:4200", "http://localhost:8083"));
         corsConfig.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         corsConfig.setAllowedHeaders(List.of("Authorization", "Content-Type"));
 
